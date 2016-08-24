992b38ae07a722ee4da81be7dea84fb29e160232
kriswithank <kristopherswann@gmail.com>
2016-08-23 22:16:11 -0500

    Copied old faqs to new faqs in database

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,4 +1,4 @@
-from .models import FaqOld, FaqCategory
+from .models import Faq, FaqOld, FaqCategory
 from django.contrib import admin
 
 class FaqsOldInline(admin.StackedInline):
@@ -6,12 +6,18 @@ class FaqsOldInline(admin.StackedInline):
     extra = 1
     ordering = ['position', 'question_text']
 
+class FaqsInline(admin.StackedInline):
+    model = Faq
+    extra = 1
+    ordering = ['position', 'title']
+    fields = ['position', 'title', 'body_markdown']
+
 class FaqCategoryAdmin(admin.ModelAdmin):
     fields = ('position', 'display_text')
     list_display = ('position', 'display_text')
     list_display_links = ('display_text',)
     list_editable = ('position',)
-    inlines = [FaqsOldInline]
+    inlines = [FaqsOldInline, FaqsInline]
     ordering = ['position', 'display_text']
 
 admin.site.register(FaqCategory, FaqCategoryAdmin)
