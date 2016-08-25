4ab1ff49ccd149e52ab790e1625ce5d597fec18e
kriswithank <kristopherswann@gmail.com>
2016-08-24 23:46:14 -0500

    Add equipment to admin site

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,3 +1,19 @@
+from .models import Equipment, EquipmentGalleryImage, EquipmentLongDescriptionSection
+from common.models import BasicPost
 from django.contrib import admin
 
-# Register your models here.
+
+class GalleryImageInline(admin.StackedInline):
+    model = EquipmentGalleryImage
+    extra = 1
+
+class LongDescriptionInline(admin.StackedInline):
+    model = EquipmentLongDescriptionSection
+    extra = 1
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    inlines = [LongDescriptionInline, GalleryImageInline]
+
+
+admin.site.register(Equipment, EquipmentAdmin)
