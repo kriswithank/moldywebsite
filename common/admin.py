ca72d6625af2a4173f483eb0c56f07b0f9cd6346
kriswithank <kristopherswann@gmail.com>
2016-08-24 21:30:26 -0500

    Remove unused import

--- common/admin.py
+++ common/admin.py
@@ -1,7 +1,5 @@
 from .models import BasicPost, TitledBasicPost, Image, NavMenuItem
-from django import forms
 from django.contrib import admin
-from django.db import models
 
 class NavMenuItemAdmin(admin.ModelAdmin):
     fields = ('position', 'display_text', 'reference_url_name')
