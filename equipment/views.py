a42a48ac4d5f146d64c856799d2e95dc930e4028
kriswithank <kristopherswann@gmail.com>
2016-09-02 20:20:49 -0500

    Add basic list view to equipment page
    Started using SASS, currently only used for the css of the equipment
    page. Template is very basic, will **have** to be changed later.

--- equipment/views.py
+++ equipment/views.py
@@ -1,4 +1,6 @@
+from .models import Equipment
 from common.views import basicPage
 
 def index(request):
-    return basicPage(request, 'equipment/base_equipment.html', {})
+    context = {'equipment': Equipment.objects.all()}
+    return basicPage(request, 'equipment/base_equipment.html', context)
