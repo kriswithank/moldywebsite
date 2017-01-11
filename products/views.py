3ff9ddee5690856c92e2d2dd6562f0d769210b0c
kriswithank <kristopherswann@gmail.com>
2017-01-10 22:46:35 -0600

    Rearrange view urls and add product detail view

--- products/views.py
+++ products/views.py
@@ -6,3 +6,10 @@ def index(request):
         'products': Product.objects.all(),
     }
     return basicPage(request, 'products/base_products.html', context)
+
+
+def detail(request, product_slug):
+    context = {
+        'product': Product.objects.get(slug=product_slug),
+    }
+    return basicPage(request, 'products/product_detail.html', context)
