0d5d34f404d82a7fe58709e3785ad9b6cc1d7fc7
kriswithank <kristopherswann@gmail.com>
2017-01-11 10:08:48 -0600

    Use url include statements for all apps and update referenced urls

--- moldyWebsite/urls.py
+++ moldyWebsite/urls.py
@@ -4,22 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import RedirectView
-from about.views import index as about_index
-from contact.views import index as contact_index
-from equipment.views import index as equipment_index
-from faqs.views import index as faqs_idex
-from home.views import index as home_index
-from reference.views import index as reference_index
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url=reverse_lazy('home'), permanent=False)),
-    url(r'^about/$', about_index, name='about'),
-    url(r'^contact/$', contact_index, name='contact'),
-    url(r'^equipment/$', equipment_index, name='equipment'),
-    url(r'^faqs/$', faqs_idex, name='faqs'),
-    url(r'^home/$', home_index, name='home'),
+    url(r'^about/', include('about.urls')),
+    url(r'^contact/', include('contact.urls')),
+    url(r'^equipment/', include('equipment.urls')),
+    url(r'^faqs/', include('faqs.urls')),
+    url(r'^home/', include('home.urls')),
     url(r'^products/', include('products.urls')),
-    url(r'^reference/$', reference_index, name='reference'),
+    url(r'^reference/', include('reference.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- reference/urls.py
+++ reference/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.conf.urls import url
 
-app_name = 'products'
+app_name = 'reference'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<product_slug>[\w-]+)/$', views.detail, name='detail'),
 ]
