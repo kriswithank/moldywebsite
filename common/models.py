5ba46463d8f85cafb9b1c4a1a059ff08d6cab05a
kriswithank <kristopherswann@gmail.com>
2017-01-15 10:55:52 -0600

    Fix save and convert_markdown methods

--- common/models.py
+++ common/models.py
@@ -48,7 +48,7 @@ class MarkdownModel(models.Model):
 
     def convert_markdown(self):
         """
-        Converts the body_markdown to html. This code is a modified version of
+        Converts the markdown to html. This code is a modified version of
         Ole Morten Halvorsen's implementation, found here:
         http://www.omh.cc/blog/2008/aug/18/django-inserting-and-positioning-images/
         """
@@ -58,8 +58,8 @@ class MarkdownModel(models.Model):
             image_url = image.image.url
             image_refs += '\n[{0}]: {1}'.format(image.name, image_url)
 
-        return markdown.markdown('{0}\n{1}'.format(self.body_markdown, image_refs))
+        return markdown.markdown('{0}\n{1}'.format(self.markdown, image_refs))
 
     def save(self):
-        self.body_html = self.convert_markdown()
+        self.html = self.convert_markdown()
         super(MarkdownModel, self).save()
